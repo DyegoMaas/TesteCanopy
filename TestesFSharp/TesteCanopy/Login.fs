4fefd200cd1930b68c77ae0f45d5b3deb9235a3d
Holmed <dyego.maas@gmail.com>
2014-05-07 01:55:13 -0300

    Adicionado o Canopy e criado um pequeno teste.

---
+++ TestesFSharp/TesteCanopy/Login.fs
@@ -0,0 +1,19 @@
+﻿namespace TestesMyrp
+
+module Login =
+
+    open canopy
+    open runner
+    open System
+
+    let urlMyrp = "https://www.myrp.com.br/testar"
+
+    //start an instance of the chrome browser
+    start chrome
+
+    let rodarTestes = fun _ ->
+        "logando no Myrp" &&& fun _ ->
+            url urlMyrp
+
+        "logando no Myrp 2" &&& fun _ ->
+            url "www.google.com"
